25b6b7c354e01c5b331170a7287b846f6196c23f
Vladimir <vldzhv@gmail.com>
2023-05-30 12:13:49 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 13676;
+        int rublesPerMile = 20;
+
+        int bonusMiles = (ticketPrice / rublesPerMile);
+
+        System.out.println("За купленный билет начислено " + bonusMiles + " миль");
+
+
+
+
+        }
+    }
